a0f77eee1ea1e3805af4546f82fff801fc03b549
oscarthorwid <id15otd@cs.umu.se>
2017-11-01 17:31:01 +0100

    metoder i Player

--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,28 @@
  * Created by oscarthorwid on 2017-11-01.
  */
 public class Player {
+    private char color(){
+
+    }
+    private String name(){
+
+    }
+    private int score(){
+
+    }
+    public Player(){
+
+    }
+    public char getColor(){
+
+    }
+    public String getName(){
+
+    }
+    public int getScore(){
+
+    }
+    public void setScore(){
+
+    }
 }
