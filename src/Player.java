0e2ad6e5a77ddbb87214008edc98e98ee5a4e5af
Petter Poucette <id15ppe@cs.umu.se>
2017-11-03 13:40:31 +0100

    Added returns in Player

--- src/Player.java
+++ src/Player.java
@@ -2,26 +2,39 @@
  * Created by oscarthorwid on 2017-11-01.
  */
 public class Player {
-    private char color(){
 
+    private char color(){
+        char letter = 't';
+
+        return letter;
     }
     private String name(){
+        String string = "test";
 
+        return string;
     }
     private int score(){
+        int tal = 1;
 
+        return tal;
     }
-    public Player(){
+    public void Player(){
 
     }
     public char getColor(){
+        char letter = 't';
 
+        return letter;
     }
     public String getName(){
+        String string = "test";
 
+        return string;
     }
     public int getScore(){
+        int tal = 1;
 
+        return tal;
     }
     public void setScore(){
 
